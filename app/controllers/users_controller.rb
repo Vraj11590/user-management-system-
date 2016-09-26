397cf0a0c6f665af39246c593deb4e827a9f3254
vj11590 <vj11590@gmail.com>
2016-09-26 04:51:21 +0000

    Sign Up Successfull Need to redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
-  def new
-  end
 
+  def new
+    @user = User.new
+  end
 
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(allowed_user_params_whitelisted)
+    if @user.save
+      flash[:success] = "Weclome #{@user.name}"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+      def allowed_user_params_whitelisted
+          params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: {maximum: 40}
 
-
-
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
              length:{maximum: 255},
